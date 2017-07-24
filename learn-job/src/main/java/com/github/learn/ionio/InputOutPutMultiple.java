f9406c41ae0b8b006a47a91d6276048bac40e362
shunzhong.deng <shunzhong.deng@gmail.com>
2017-07-24 10:08:44 +0800

    分离ionio与multiplethread 包

---
+++ learn-job/src/main/java/com/github/learn/ionio/InputOutPutMultiple.java
@@ -0,0 +1,73 @@
+package com.github.learn.ionio;
+
+class Resource {
+	private boolean flag = false;
+	private int count = 0;
+
+	public synchronized void addResource() {
+		while (this.flag) {
+			try{
+				this.wait();
+			} catch(Exception e) {
+			}
+		}
+		System.out.println(Thread.currentThread().getName() + " ->->->->->-> add.............." + ++count);
+		flag = true;
+		this.notifyAll();
+	} 	
+	
+	public synchronized void useResource() {
+		while (!this.flag) {
+			try{
+				this.wait();
+			} catch(Exception e) {
+			} 
+		}
+			
+		System.out.println(Thread.currentThread().getName() + " <<<<<<< use....." + --count);
+		flag = false;
+		this.notifyAll();
+
+	}
+}
+
+
+class Input implements Runnable {
+	
+	private Resource res;
+	Input(Resource res) {
+		this.res = res;
+	}
+	public void run() {
+		while (true) {
+			res.addResource();
+		}
+	}
+	
+}
+
+class Output implements Runnable {
+	
+	private Resource res;
+	Output(Resource res) {
+		this.res = res;
+	}
+	public void run() {
+		while (true) {			
+			res.useResource();
+		}		
+	}
+}
+
+
+class InputOutPutMultiple {
+	public static void main(String[] args) {
+		
+		Resource res = new Resource();
+		new Thread(new Input(res)).start();
+		new Thread(new Input(res)).start();
+		new Thread(new Output(res)).start();
+		new Thread(new Output(res)).start();
+
+	}
+}
---
+++ learn-job/src/main/java/com/github/learn/multiplethread/future/Data.java
@@ -0,0 +1,5 @@
+package com.github.learn.multiplethread.future;
+
+public interface Data {  
+    String getResult() throws InterruptedException;  
+}
