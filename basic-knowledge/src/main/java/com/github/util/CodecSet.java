765ebd33a8c90bc9c27d0ff75a372f6d3d65a052
shunzhong <dengshunzhongwj63132@touna.cn>
2019-11-13 17:24:03 +0800

    设计模式

--- basic-knowledge/src/main/java/com/github/util/CodecSet.java
+++ basic-knowledge/src/main/java/com/github/util/CodecSet.java
@@ -3,6 +3,4 @@ package com.github.util;
 public  interface CodecSet {
    Encoder getEncoder(String encodingName);
    Decoder getDecoder(String encodingName);
-
-
 }
--- basic-knowledge/src/main/java/com/github/util/StandardCodecs.java
+++ basic-knowledge/src/main/java/com/github/util/StandardCodecs.java
@@ -1,14 +1,15 @@
 package com.github.util;
 
+import java.util.Iterator;
 import java.util.ServiceLoader;
 
 public class StandardCodecs implements CodecSet {
     private static ServiceLoader<CodecSet> codecSetLoader  =  ServiceLoader.load(CodecSet.class);
 
     @Override
-    public Encoder getEncoder(String encodingName) {
+    public Encoder getEncoder(String mysql) {
         for (CodecSet cp : codecSetLoader) {
-            Encoder enc = cp.getEncoder(encodingName);
+            Encoder enc = cp.getEncoder(mysql);
             if (enc != null)
                 return enc;
         }
